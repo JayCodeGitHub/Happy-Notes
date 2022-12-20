daf40ba0a6cfa5600f5eb21640837c2bf92f7bec
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-21 00:27:28 +0100

    Created Card Component

--- client/src/views/Notes.tsx
+++ client/src/views/Notes.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Card from '../components/Card/Card'
 
 function Notes() {
   return (
     <div>
       <h1 className='text-3xl font-bold underline text-red-500'>Notes</h1>
+      <Card />
     </div>
   )
 }
