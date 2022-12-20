563c91ce4c0fe4cca6b6b15883957175cba493b6
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-20 03:21:50 +0100

    Organized file structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import './assets/styles/index.css'
 import Root from './views/Root'
 import reportWebVitals from './reportWebVitals'
 
