62d3d30c9971c90d32709e5cf2089a1d1d023097
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-21 00:47:59 +0100

    Render Notes with card

--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,10 +1,44 @@
 import React from 'react'
 
-function Card() {
+interface CardProps {
+  type: string
+  title: string
+  body?: string
+}
+
+function Card({ type, title, body }: CardProps) {
   return (
-    <div>
-      <h1 className=''>Card</h1>
-    </div>
+    <>
+      {type === 'note' ? (
+        <>
+          <div>
+            <div>
+              <h1>{title}</h1>
+            </div>
+            <div>
+              <p>{body}</p>
+              <button>REMOVE</button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <div>
+              <h1>{title}</h1>
+            </div>
+            <div>
+              <button>REMOVE</button>
+              {type === 'site' ? (
+                <a target='_blank' href={title} rel='noopener noreferrer'>
+                  <button>Go to side</button>
+                </a>
+              ) : null}
+            </div>
+          </div>
+        </>
+      )}
+    </>
   )
 }
 
--- client/src/views/Notes.tsx
+++ client/src/views/Notes.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '../components/Card/Card'
 
 function Notes() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      title: 'Hello World',
+      body: 'Lorem Ipsum',
+    },
+    {
+      id: 2,
+      title: 'Hello World',
+      body: 'Lorem Ipsum',
+    },
+  ])
   return (
     <div>
       <h1 className='text-3xl font-bold underline text-red-500'>Notes</h1>
-      <Card />
+      {notes.map(({ id, title, body }) => (
+        <Card title={title} body={body} type='note' key={id} />
+      ))}
     </div>
   )
 }
