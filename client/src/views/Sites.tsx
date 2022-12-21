3ca59ef60a8dfe488a71f47dc7ef023d93b514ad
JayCodeGitHub <JayCode.contakt@gmail.com>
2022-12-21 01:33:05 +0100

    Render all types of Notes with card

--- client/src/views/Sites.tsx
+++ client/src/views/Sites.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from '../components/Card/Card'
 
 function Sites() {
+  const [sites, setSites] = useState([
+    {
+      id: 1,
+      title: 'https://www.facebook.com/',
+    },
+    {
+      id: 2,
+      title: 'https://www.instagram.com/',
+    },
+  ])
   return (
     <div>
-      <h1>Sites</h1>
+      <h1 className='text-3xl font-bold underline text-red-500'>Sites</h1>
+      {sites.map(({ id, title }) => (
+        <Card title={title} type='site' key={id} />
+      ))}
     </div>
   )
 }
--- client/src/views/ToDo.tsx
+++ client/src/views/ToDo.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Card from '../components/Card/Card'
 
 function ToDo() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'shopping',
+    },
+    {
+      id: 2,
+      title: 'yoga',
+    },
+  ])
   return (
     <div>
-      <h1>ToDo</h1>
+      <h1 className='text-3xl font-bold underline text-red-500'>ToDos</h1>
+      {todos.map(({ id, title }) => (
+        <Card title={title} type='todo' key={id} />
+      ))}
     </div>
   )
 }
