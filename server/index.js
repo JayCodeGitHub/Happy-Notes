89d7db6c3ade3d2e9b28566ab3f2c2118c83138f
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-01-07 23:49:08 +0100

    Created simple Express Server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
-console.log('hello world')
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res) {
+    res.json({
+        'status': 'Success!'
+    });
+});
+
+app.listen(8080, function() {
+    console.log('Listening!');
+})
