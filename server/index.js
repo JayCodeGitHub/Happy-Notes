b5d0119699f2f6f97095c98745d7bed23a847eeb
JayCodeGitHub <JayCode.contakt@gmail.com>
2023-01-11 15:28:45 +0100

    Connected server to mongodb

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const Router = require('./src/routes/index')
 
 const app = express();
 
 app.use('/', Router)
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017', { useNewUrlParser: true, useUnifiedTopology: true});
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
 
 const port = 8080
 
